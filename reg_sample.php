5c9f7b980a9848c02dce4afe1c0bd2885b988aac
EyadBelal <70587112+EyadBelal@users.noreply.github.com>
2020-09-29 21:02:44 -0400

    Update reg_sample.php

--- reg_sample.php
+++ reg_sample.php
@@ -4,48 +4,57 @@
   <input type="email" id="email" name="email" required/>
   <label for="p1">Password:</label>
   <input type="password" id="p1" name="password" required/>
-  <input type="submit" name="login" value="Login"/>
+  <label for="p2">Confirm Password:</label>
+  <input type="password" id="p2" name="confirm" required/>
+  <input type="submit" name="register" value="Register"/>
 </form>
-
 <?php
-if(isset($_POST["login"])){
+if(isset($_POST["register"])){
   $email = null;
   $password = null;
+  $confirm = null;
   if(isset($_POST["email"])){
     $email = $_POST["email"];
   }
   if(isset($_POST["password"])){
     $password = $_POST["password"];
   }
+  if(isset($_POST["confirm"])){
+    $confirm = $_POST["confirm"];
+  }
   $isValid = true;
-  if(!isset($email) || !isset($password)){
+  //check if passwords match on the server side
+  if($password == $confirm){
+    echo "Passwords match <br>"; 
+  }
+  else{
+    echo "Passwords don't match<br>";
+    $isValid = false;
+  }
+  if(!isset($email) || !isset($password) || !isset($confirm)){
    $isValid = false; 
   }
   //TODO other validation as desired, remember this is the last line of defense
-  //here you'd probably want some email validation, for sake of example let's do a super basic one
-  if(!strpos($email, "@")){
-   $isValid = false;
-    echo "<br>Invalid email<br>";
-  }
   if($isValid){
-    //for password matching, we can't use this, every time it's ran it'll be a different value
-    //so will never log us in!
-    //$hash = password_hash($password, PASSWORD_BCRYPT);
-    //instead we'll want to run password_verify
-    //TODO pretend we got our use from the DB
-    //make sure if you're pasting a sample hash here that you use single quotes
-    //if you use double quotes it'll try to parse values with $ as a php variable
-    //and the sample won't work
-    $password_hash_from_db = '';//placeholder, you can copy/paste a hash generated from sample_reg.php if you want to test it
-    //otherwise it'll always be false
-    
-    //note it's raw password, saved hash as the parameters
-    if(password_verify($password, $password_hash_from_db)){
-     echo "<br>Welcome! You're logged in!<br>"; 
-    }
-    else{
-     echo "<br>Invalid password, get out!<br>"; 
-    }
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+    require_once("db.php");
+    $db = getDB();
+	if(isset($db)){
+		//here we'll use placeholders to let PDO map and sanitize our data
+		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+		//here's the data map for the parameter to data
+		$params = array(":email"=>$email, ":password"=>$hash);
+		$r = $stmt->execute($params);
+		//let's just see what's returned
+		echo "db returned: " . var_export($r, true);
+		$e = $stmt->errorInfo();
+		if($e[0] == "00000"){
+			echo "<br>Welcome! You successfully registered, please login.";
+		}
+		else{
+			echo "uh oh something went wrong: " . var_export($e, true);
+		}
+	}
   }
   else{
    echo "There was a validation issue"; 
