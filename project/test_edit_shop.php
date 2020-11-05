3065895227dcf3b43b6accb610fe1b71ac9aa136
EyadBelal <70587112+EyadBelal@users.noreply.github.com>
2020-11-05 13:22:41 -0500

    Update test_edit_shop.php

--- project/test_edit_shop.php
+++ project/test_edit_shop.php
@@ -1,13 +1,63 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if(isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+?>
+<?php
+//saving
+if(isset($_POST["save"])){
+	//TODO add proper validation/checks
+    $name = $_POST["name"];
+    $amount = $_POST["quantity"];
+    $cost= $_POST["price"];
+    $desc = $_POST["description"];
+    $nst = date('Y-m-d H:i:s');//calc
+    $user = get_user_id();
+    $db = getDB();
+    if(isset($id)){
+        $stmt = $db->prepare("UPDATE Products set name=:name, amount=:quantity, cost=:price, desc=description");
+        $quantity = $amount;
+        $price = $cost;
+        $description = $desc;
+        $r = $stmt->execute([
+            ":name"=>$name,
+            ":amount"=>$quantity,
+            ":cost"=>$price,
+            ":desc"=>$description,
+            ":user"=>$user,
+        ]);
+        if($r){
+            flash("Updated successfully with id: " . $id);
+        }
+        else{
+            $e = $stmt->errorInfo();
+            flash("Error updating: " . var_export($e, true));
+        }
+    }
+    else{
+        flash("ID isn't set, we need an ID in order to update");
+    }
+}
+?>
 <?php
 //fetching
 $result = [];
 if(isset($id)){
     $id = $_GET["id"];
     $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM products where id = :id");
+    $stmt = $db->prepare("SELECT * FROM Products where id = :id");
     $r = $stmt->execute([":id"=>$id]);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC)
-        }
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
 ?>
 <form method="POST">
     <label>Name</label>
@@ -22,13 +72,8 @@ if(isset($id)){
     <label> Description </label>
     <input type="text" desc="" name="description"/>
 
-    <label> Modified </label>
-    <input type="time" mod="current_time_stamp" name="modified"/>
-
-    <label> Created </label>
-    <input type="time" crea="current_time_stamp" name="created"/>
-
     <input type="submit" name="save" value="create"/>
 </form>
+
 <?php require(__DIR__ . "/partials/flash.php");
 
